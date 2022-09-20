94f576d03755a42a7edd0fd9ea1adb33ed0dc91f
hyeonmando <jeong010923@naver.com>
2022-09-21 01:53:55 +0900

    카카오 키해쉬 다시

---
+++ app/src/main/java/com/example/teamm/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.teamm;
+
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.mf.map.api.MapView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+    }
+}
